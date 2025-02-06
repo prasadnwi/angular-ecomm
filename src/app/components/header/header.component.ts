789edb595d87e104ef8549477d35929c561f70f5
isuru madhuwantha <prasadnwip@gmail.com>
2025-02-06 13:22:33 +0530

    adding tailwind

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <div class="bg-slate-100 px-4 py-3 shadow-md">
+     <span>My Store</span>
+     </div>`,
+  styles: ``
+})
+export class HeaderComponent {
+
+}
